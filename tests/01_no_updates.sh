6a4d925ff948064b718bffb1bea6cb6e443d0702
Jonathan Wright <jonathan.wright@devopsguys.com>
2016-06-15 16:54:05 +0100

    Start adding support for unit tests of the script

---
+++ tests/01_no_updates.sh
@@ -0,0 +1,9 @@
+# Emulate OpenVPN environment
+script_type="up"
+dev="tun01"
+
+TEST_IFINDEX="$((RANDOM%=64))"
+TEST_TITLE="No Updates"
+TEST_BUSCTL_CALLED=0
+TEST_BUSCTL_DNS=""
+TEST_BUSCTL_DOMAINS=""
---
+++ tests/02_single_ipv4_dns.sh
@@ -0,0 +1,10 @@
+# Emulate OpenVPN environment
+script_type="up"
+dev="tun02"
+foreign_option_1="dhcp-option DNS 1.23.4.56"
+
+TEST_IFINDEX="$((RANDOM%=64))"
+TEST_TITLE="Single IPv4 DNS Server"
+TEST_BUSCTL_CALLED=1
+TEST_BUSCTL_DNS="1 2 4 1 23 4 56"
+TEST_BUSCTL_DOMAINS=""

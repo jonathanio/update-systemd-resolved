eda48bb86c4af80e1bbd29086dd96e19fd139431
Jonathan Wright <jonathan.wright@devopsguys.com>
2016-06-15 19:53:16 +0100

    Add additional tests for DNS and Domains

---
+++ tests/08_dns_ipv4_and_domain.sh
@@ -0,0 +1,10 @@
+script_type="up"
+dev="tun08"
+foreign_option_1="dhcp-option DNS 1.23.4.56"
+foreign_option_2="dhcp-option DNS 2.34.56.7"
+foreign_option_3="dhcp-option DOMAIN example.com"
+
+TEST_TITLE="DNS IPv4 Servers and Domain"
+TEST_BUSCTL_CALLED=1
+TEST_BUSCTL_DNS="2 2 4 1 23 4 56 2 4 2 34 56 7"
+TEST_BUSCTL_DOMAINS="1 example.com false"
---
+++ tests/09_dns_ipv4_domain_and_search.sh
@@ -0,0 +1,12 @@
+script_type="up"
+dev="tun09"
+foreign_option_1="dhcp-option DNS 1.23.4.56"
+foreign_option_2="dhcp-option DNS 2.34.5.67"
+foreign_option_3="dhcp-option DOMAIN example.co.uk"
+foreign_option_4="dhcp-option DOMAIN-SEARCH example.co"
+foreign_option_5="dhcp-option DOMAIN-SEARCH example.com"
+
+TEST_TITLE="DNS IPv4 Servers, Domain, and Search"
+TEST_BUSCTL_CALLED=1
+TEST_BUSCTL_DNS="2 2 4 1 23 4 56 2 4 2 34 5 67"
+TEST_BUSCTL_DOMAINS="3 example.co.uk false example.co true example.com true"

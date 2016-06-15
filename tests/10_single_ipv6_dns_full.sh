d0f2af0ef2ce2f4eaba716067b91b46e008b90a9
Jonathan Wright <jonathan.wright@devopsguys.com>
2016-06-15 20:40:00 +0100

    Add first IPv6 tests

---
+++ tests/10_single_ipv6_dns_full.sh
@@ -0,0 +1,8 @@
+script_type="up"
+dev="tun10"
+foreign_option_1="dhcp-option DNS 1234:5678:90ab:cdef:4321:8765:ba09:fedc"
+
+TEST_TITLE="Single IPv6 DNS Server (Full)"
+TEST_BUSCTL_CALLED=1
+TEST_BUSCTL_DNS="1 2 16 18 52 70 120 144 171 205 239 67 33 135 101 186 9 254 220"
+TEST_BUSCTL_DOMAINS=""
---
+++ tests/11_single_ipv6_dns_simple.sh
@@ -0,0 +1,8 @@
+script_type="up"
+dev="tun11"
+foreign_option_1="dhcp-option DNS 1234:567:89:0:ab:cde:f123:4567"
+
+TEST_TITLE="Single IPv6 DNS Server (Full, Simple)"
+TEST_BUSCTL_CALLED=1
+TEST_BUSCTL_DNS="1 2 16 18 52 5 103 0 137 0 0 0 171 12 222 241 35 69 103"
+TEST_BUSCTL_DOMAINS=""

f36bfaaca7e45dab6dcedd4c990b0cf12cf2d53e
Jonathan Wright <jonathan.wright@devopsguys.com>
2016-06-15 21:09:36 +0100

    Add remaining IPv6 single-address tests

---
+++ tests/14_single_ipv6_dns_compact_3.sh
@@ -0,0 +1,8 @@
+script_type="up"
+dev="tun14"
+foreign_option_1="dhcp-option DNS 20a0:::1"
+
+TEST_TITLE="Single IPv6 DNS Server (Compact) (Part 3)"
+TEST_BUSCTL_CALLED=1
+TEST_BUSCTL_DNS="1 2 16 32 160 0 0 0 0 0 0 0 0 0 0 0 0 0 1"
+TEST_BUSCTL_DOMAINS=""

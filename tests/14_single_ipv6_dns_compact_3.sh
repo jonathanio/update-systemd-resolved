d513f961e562f39401aefdb482f7554b3b242bb2
Jonathan Wright <jonathan.wright@devopsguys.com>
2016-06-16 21:21:58 +0100

    Fix invalid address and incorrect result in tests

--- tests/14_single_ipv6_dns_compact_3.sh
+++ tests/14_single_ipv6_dns_compact_3.sh
@@ -1,6 +1,6 @@
 script_type="up"
 dev="tun14"
-foreign_option_1="dhcp-option DNS 20a0:::1"
+foreign_option_1="dhcp-option DNS 20a0::1"
 
 TEST_TITLE="Single IPv6 DNS Server (Compact) (Part 3)"
 TEST_BUSCTL_CALLED=1

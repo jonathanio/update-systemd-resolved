39f369a7555a1707a3cbb16bb555b08c1a407257
Jonathan Wright <jonathan.wright@devopsguys.com>
2016-08-24 11:26:29 +0100

    Rename test_options to test_invalids to avoid array conversion
    The invalid tests attempted to convert an associative array to an indexed array. This should avoid that issue.

--- tests/21_dnssec_invalid_options.sh
+++ tests/21_dnssec_invalid_options.sh
@@ -4,14 +4,14 @@ dev="tun21"
 TEST_BUSCTL_CALLED=0
 EXPECT_FAILURE=1
 
-declare -a test_options=(
+declare -a test_invalids=(
   'Default'
   'TRUE'
   'FALSE'
   'ALLOW-DOWNGRADE'
 )
 
-for test_option in "${!test_options[@]}"; do
+for test_option in "${!test_invalids[@]}"; do
   TEST_TITLE="DNSSEC Set to $test_option"
   foreign_option_1="dhcp-option DNSSEC $test_option"
   runtest

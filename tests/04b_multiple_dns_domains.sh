0ce7b9bc9b2b39334b4f84885b671456657a1dfd
Jonathan Wright <jon@than.io>
2016-08-29 19:37:12 +0100

    Fix handling for multiple DNS Domains
    Incorrect test 04b meant that the dns_domain_count was incremented with every DOMAIN options supplied, when it should only be set to 1 (i.e. it has a value). Added additional test as well to test alongside DOMAIN-SEARCH.

--- tests/04b_multiple_dns_domains.sh
+++ tests/04b_multiple_dns_domains.sh
@@ -1,7 +1,7 @@
 script_type="up"
 dev="tun04"
 foreign_option_1="dhcp-option DOMAIN example.com"
-foreign_option_1="dhcp-option DOMAIN example.co"
+foreign_option_2="dhcp-option DOMAIN example.co"
 
 TEST_TITLE="Multiple DNS Domains"
 TEST_BUSCTL_CALLED=1

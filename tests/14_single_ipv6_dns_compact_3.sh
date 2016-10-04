b18e3608ca0569fdcabca59cf91fef93f6fec84a
Arjen Schol <arjen@parse.nl>
2016-10-04 14:06:29 +0200

    Fix IPv6 DNS by specifying AF_INET6 value (10) insteadof array size (2)

--- tests/14_single_ipv6_dns_compact_3.sh
+++ tests/14_single_ipv6_dns_compact_3.sh
@@ -4,4 +4,4 @@ foreign_option_1="dhcp-option DNS 20a0::1"
 
 TEST_TITLE="Single IPv6 DNS Server (Compact) (Part 3)"
 TEST_BUSCTL_CALLED=1
-TEST_BUSCTL_DNS="1 2 16 32 160 0 0 0 0 0 0 0 0 0 0 0 0 0 1"
+TEST_BUSCTL_DNS="1 10 16 32 160 0 0 0 0 0 0 0 0 0 0 0 0 0 1"
--- tests/15_single_ipv6_dns_localhost.sh
+++ tests/15_single_ipv6_dns_localhost.sh
@@ -4,4 +4,4 @@ foreign_option_1="dhcp-option DNS ::1"
 
 TEST_TITLE="Single IPv6 DNS Server (Localhost)"
 TEST_BUSCTL_CALLED=1
-TEST_BUSCTL_DNS="1 2 16 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1"
+TEST_BUSCTL_DNS="1 10 16 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1"

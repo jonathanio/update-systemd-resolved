7de24b4c2ca3deca5b73db065f86b98f10026c3b
Jonathan Wright <jon@than.io>
2017-11-12 14:21:32 +0000

    Add support for ADAPTER_DOMAIN_SUFFIX command via dhcp-option
    Add support for ADAPTER_DOMAIN_SUFFIX which is the Microsoft alternative to the DOMAIN option provided by this script. Resolves issue #38.

---
+++ tests/05e_dns_domain_alternate.sh
@@ -0,0 +1,7 @@
+script_type="up"
+dev="tun05"
+foreign_option_1="dhcp-option ADAPTER_DOMAIN_SUFFIX example.org"
+
+TEST_TITLE="DNS Doamin using ADAPTER_DOMAIN_SUFFIX"
+TEST_BUSCTL_CALLED=1
+TEST_BUSCTL_DOMAINS="1 example.org false"

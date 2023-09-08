bfff74ecb644947810928c12205eee082cfdcbe9
Matt Schreiber <tomeon@dogea.red>
2023-09-08 10:54:38 -0400

    fix(nix): update pkg to deal with Makefile updates

--- nix/packages.nix
+++ nix/packages.nix
@@ -19,6 +19,8 @@
       # `make test`.
       doCheck = true;
 
+      PREFIX = placeholder "out";
+
       # Rewrite update-systemd-resolved.conf to:
       #   1. Remove "setenv PATH ..." (setting PATH is unnecessary here, where
       #      nixpkgs' update-systemd-resolved derivation builder replaces
@@ -30,12 +32,10 @@
       #      "config <nix-store-path-of>/update-systemd-resolved.conf" from
       #      within an OpenVPN config file will work properly).
       postInstall = ''
-        ${oldAttrs.postInstall or ""}
-
         sed -i -e "
           /^setenv[[:space:]]\+PATH/d
           s|\([[:space:]]\)[^[[:space:]]*\(/update-systemd-resolved\)|\1''${out}/libexec/openvpn\2|
-        " "''${out}/libexec/openvpn/update-systemd-resolved.conf"
+        " "''${out}/share/doc/openvpn/update-systemd-resolved.conf"
 
         wrapProgram "''${out}/libexec/openvpn/update-systemd-resolved" \
           --suffix PATH : ${lib.makeBinPath buildInputs}
@@ -49,8 +49,10 @@
       #
       #   /nix/store/<...>/bin/bash: line 1: ./run-tests: cannot execute: required file not found
       #
+      # Additionally, update the shebang of `update-systemd-resolved` itself in
+      # order to permit running the `--print-polkit-rules` action.
       patchPhase = ''
-        patchShebangs ./run-tests
+        patchShebangs ./run-tests ./update-systemd-resolved
       '';
 
       passthru = {
